00279e594c2c21f767b575545e76dc1db771af10
Areeba Zahir <areebazahir3@gmail.com>
2020-08-23 22:51:03 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -22,15 +22,3 @@ def speak_to_grandma(string)
     return "NO, NOT SINCE 1938!"
     end
 end 
-
-def speak_to_grandma(string)
-  if string == string.upcase
-    return "NO, NOT SINCE 1938!"
-  elsif string != string.upcase
-    return  "HUH?! SPEAK UP, SONNY!"
-  elsif string == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
-    end
-end 
-
-speak_to_grandma("I LOVE YOU GRANDMA!")
